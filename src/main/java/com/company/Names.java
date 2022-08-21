d1dba482d34341e6775ebe24b332bad7bb8d6e6e
Olha <olgaklim510@gmail.com>
2022-08-21 12:17:12 +0300

    Changed class Names and class Numbers

--- src/main/java/com/company/Names.java
+++ src/main/java/com/company/Names.java
@@ -1,9 +1,8 @@
 package com.company;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 
 class Names {
@@ -11,16 +10,11 @@ class Names {
                                                        "Oleg", "Taras");
 
     public static void main(String[] args) {
-        System.out.println(check(NAMES, i -> i % 2 != 0));
-    }
+        String result = NAMES.stream()
+                .filter(name -> NAMES.indexOf(name) % 2 != 0)
+                .map(name -> NAMES.indexOf(name) + ". " + name)
+                .collect(Collectors.joining(", "));
 
-    private static List<String> check(List<String> names, Predicate <Integer> checkIndex) {
-        List<String> result = new ArrayList<>();
-        for (int i = 0; i < names.size(); i++) {
-            if (checkIndex.test(i)) {
-                result.add(i + ". " + names.get(i));
-            }
-        }
-        return result;
+        System.out.println(result);
     }
 }
--- src/main/java/com/company/Numbers.java
+++ src/main/java/com/company/Numbers.java
@@ -1,17 +1,19 @@
 package com.company;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 class Numbers {
     private static final List<String> STRING_NUMBERS = Arrays.asList("1,2,0", "4,5");
 
     public static void main(String[] args) {
-        List<Integer> result= STRING_NUMBERS.stream()
+        String result= STRING_NUMBERS.stream()
                 .map(s -> s.split(","))
                 .flatMap(Arrays::stream)
                 .map(Integer::valueOf)
                 .sorted()
-                .toList();
+                .map(num->String.valueOf(num))
+                .collect(Collectors.joining(", "));
         System.out.println(result);
     }
 }
